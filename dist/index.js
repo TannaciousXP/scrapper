56cef8b95792977ecfa94316446adff0c48cd3c5
Peter Tan <pxhtan@gmail.com>
2018-01-11 16:35:24 -0800

    scrape and trending func completed

--- dist/index.js
+++ dist/index.js
@@ -20,20 +20,146 @@ var _dotenv = require('dotenv');
 
 var _dotenv2 = _interopRequireDefault(_dotenv);
 
+var _twit = require('twit');
+
+var _twit2 = _interopRequireDefault(_twit);
+
+var _babelPolyfill = require('babel-polyfill');
+
+var _babelPolyfill2 = _interopRequireDefault(_babelPolyfill);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_dotenv2.default.config(); // const express = require('express');
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; } // const express = require('express');
 // const path = require('path');
 // const bodyParser = require('body-parser');
 // const axios = require('axios');
 // const dotenv = require('dotenv').config();
+
+_dotenv2.default.config();
+
 
 var app = (0, _express2.default)();
 
 app.use(_bodyParser2.default.json());
 app.use(_bodyParser2.default.urlencoded({ extended: true }));
 
-console.log('.env: TEST:' + process.env.TEST);
+var sf_woeid = 2487956;
+
+var T = new _twit2.default({
+  consumer_key: process.env.C_KEY,
+  consumer_secret: process.env.C_SECRET,
+  access_token: process.env.M_KEY,
+  access_token_secret: process.env.M_SECRET
+});
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1; //January is 0!
+var yyyy = today.getFullYear();
+
+if (dd < 10) {
+  dd = '0' + dd;
+}
+
+if (mm < 10) {
+  mm = '0' + mm;
+}
+
+today = yyyy + '-' + mm + '-' + dd;
+
+// sort by tweet_volume
+
+var getTrendsByPlace = function getTrendsByPlace(woeid) {
+  return new Promise(function (res) {
+    T.get('trends/place', { id: sf_woeid }, function (err, data, res) {
+      var sortedData = data[0].trends.sort(function (a, b) {
+        return b.tweet_volume - a.tweet_volume;
+      });
+      res(sortedData);
+    });
+  });
+};
+
+var testing = function () {
+  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+    var test, _iteratorNormalCompletion, _didIteratorError, _iteratorError, _iterator, _step, ele;
+
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.next = 2;
+            return getTrendsByPlace(sf_woeid);
+
+          case 2:
+            test = _context.sent;
+            _iteratorNormalCompletion = true;
+            _didIteratorError = false;
+            _iteratorError = undefined;
+            _context.prev = 6;
+
+            for (_iterator = test[Symbol.iterator](); !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+              ele = _step.value;
+
+              console.log(ele);
+            }
+            _context.next = 14;
+            break;
+
+          case 10:
+            _context.prev = 10;
+            _context.t0 = _context['catch'](6);
+            _didIteratorError = true;
+            _iteratorError = _context.t0;
+
+          case 14:
+            _context.prev = 14;
+            _context.prev = 15;
+
+            if (!_iteratorNormalCompletion && _iterator.return) {
+              _iterator.return();
+            }
+
+          case 17:
+            _context.prev = 17;
+
+            if (!_didIteratorError) {
+              _context.next = 20;
+              break;
+            }
+
+            throw _iteratorError;
+
+          case 20:
+            return _context.finish(17);
+
+          case 21:
+            return _context.finish(14);
+
+          case 22:
+          case 'end':
+            return _context.stop();
+        }
+      }
+    }, _callee, undefined, [[6, 10, 14, 22], [15,, 17, 21]]);
+  }));
+
+  return function testing() {
+    return _ref.apply(this, arguments);
+  };
+}();
+
+testing();
+
+// T.get('search/tweets', { q: `tech since:${today}`, count: 5 }, function (err, data, response) {
+//   console.log(data)
+// })
+
+
+// T.get('users/suggestions/:slug', { slug: 'twitter'}, (err, data, res) => {
+//   console.log(data);
+// });
 
 var PORT = process.env.PORT || 3000;
 
--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
-// const express = require('express');
-// const path = require('path');
-// const bodyParser = require('body-parser');
-// const axios = require('axios');
-// const dotenv = require('dotenv').config();
-
 import express from 'express';
-import path from 'path';
 import bodyParser from 'body-parser';
-import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config();
+import Twit from 'twit';
+import pollyFill from 'babel-polyfill';
+import request from 'request';
+import cheerio from 'cheerio';
+
+
+import path from 'path';
+import fs  from 'fs';
+import csv from 'fast-csv';
+
 
 const app = express();
 
@@ -17,8 +19,142 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Twitter Auth, tokens are located in the .env file
+const T = new Twit({
+  consumer_key: process.env.C_KEY,
+  consumer_secret: process.env.C_SECRET,
+  access_token: process.env.M_KEY,
+  access_token_secret: process.env.M_SECRET
+});
 
-console.log(`.env: TEST:${process.env.TEST}`)
+// Set up write stream
+let ws = fs.createWriteStream(path.join(__dirname, '../topics.csv'));
+
+// get SF woeID
+const sf_woeid = 2487956;
+// Find trends, this function finds trends for sf_woeid
+const getTrendsByPlace = (woeid) => {
+  return new Promise((res) => {    
+    T.get('trends/place', { id: sf_woeid }, (err, data, response) => {
+      let sortedData = data[0].trends.sort((a, b) => b.tweet_volume - a.tweet_volume);
+      res(sortedData);
+      // res(data);
+    });
+  });
+};
+
+/*
+Reuseable function for scraping Reddit subredits, returns an array with elements structured:
+[rank, popularity, topic, url];
+*/
+const scrape = ($) => {
+  const csvArr = [];
+  let count = 1;
+  $('.thing', '#siteTable').each(function () {
+    
+    let tableEle = this.attribs;
+    let fullName = tableEle['data-fullname'].substr(3);
+    let idx = tableEle['data-permalink'].lastIndexOf(fullName) + 7;
+    let title = tableEle['data-permalink'].substr(idx);
+
+    title = title.replace(/_/g, ' ');
+    title = title.substr(0, title.length - 1);
+
+    const url = tableEle['data-url'][0] === 'h' ? tableEle['data-url'] : 'No Url';
+
+    csvArr.push([0, tableEle['data-score'], title, url]);
+  });
+
+  csvArr.sort((a, b) => b[1] - a[1]);
+  csvArr.forEach(ele => {
+    ele[0] = count;
+    count++;
+  });
+  return csvArr;
+};
+
+// scrapes the Reddit Technology page
+const scrapeSubReddit = (topic) => {
+  return new Promise ((res) => {
+    request(`https://www.reddit.com/r/${topic}/`, (err, resp, body) => {
+      let data;
+      if (!err && resp.statusCode === 200) {
+        let $ = cheerio.load(body);
+        data = scrape($);
+      }
+      res(data);
+    });
+  });
+}
+
+
+
+// Get today's date for searching tweets
+const getToday = () => {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1; //January is 0!
+  var yyyy = today.getFullYear();
+  
+  if (dd < 10) {
+    dd = '0' + dd
+  }
+  
+  if (mm < 10) {
+    mm = '0' + mm
+  }
+  
+  today = `${yyyy}-${mm}-${dd}`;
+  return today;
+};
+
+const searchTweets = (topic) => {
+  const topicObj = {};
+  return new Promise((res) => {
+    T.get('search/tweets', { q: `${topic} since:${getToday()}`, count: 5 }, (err, data, response) => {
+      data.statuses = data.statuses.sort((a, b) => b.retweet_count - a.retweet_count);
+      console.log(data.statuses);
+      // console.log(data.statuses[0].entities);
+      // let url = `https://twitter.com/${}/status/${}`
+    })
+
+  });
+}
+
+
+/*
+Get trends from Twitter, scrapes Reddit Tech and Business page
+pipes to the csv file separating the by "- Source"
+*/
+const pipeToCSV = async () => {
+  let data = [];
+  let count = 1;
+  let trends = await getTrendsByPlace(sf_woeid);
+  let tech = await scrapeSubReddit('technology');
+  let business = await scrapeSubReddit('business');
+  data.push(['Rank', 'Popularity', 'Topic', 'Link']);
+  data.push(['-', 'TWITTER TRENDS']);
+  for (let ele of trends) {
+    data.push([count, ele.tweet_volume === null ? 0 : ele.tweet_volume, ele.name, ele.url]);
+    count++;
+  }
+  data.push(['-', 'REDDIT TECH']);
+  for (let ele of tech) {
+    data.push(ele);
+  }
+  data.push(['-', 'REDDIT BUSINESS']);
+  for (let ele of business) {
+    data.push(ele);
+  }
+  csv.write(data, { headers: true }).pipe(ws);
+}
+
+
+
+// pipeToCSV();
+
+
+
 
 
 const PORT = process.env.PORT || 3000;
